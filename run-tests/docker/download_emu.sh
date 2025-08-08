648a687efde745131f5635b297b3a966ee5713e7
Andrea Ciliberti <meziu210@icloud.com>
2025-08-08 09:18:10 +0200

    Not working

--- run-tests/docker/download_emu.sh
+++ run-tests/docker/download_emu.sh
@@ -6,7 +6,9 @@ TAG=$1
 
 curl "https://api.github.com/repos/azahar-emu/azahar/releases/tags/${TAG}" |
     jq --raw-output '.assets[].browser_download_url' |
-    grep -E 'azahar.AppImage' |
-    xargs wget -O azahar.AppImage
+    grep -E 'azahar.*.appimage.tar.gz' |
+    xargs wget -O azahar-linux.tar.gz
+
+tar --strip-components 1 -xvf azahar-linux.tar.gz
 
 chmod a+x azahar.AppImage
